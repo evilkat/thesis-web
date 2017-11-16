b6d4a9a9ede64a60019f19ae6d06a175ac246f30
Kat <theevilkat93@gmail.com>
2017-11-16 17:14:35 +0800

    added working chart code to be merged with mongodb later

--- hello.py
+++ hello.py
@@ -1,38 +1,51 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
-from wtforms import Form, TextField, TextArea, validators, StringField, SubmitField
+from flask import Flask, flash, render_template, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+#import pandas_highcharts
+from flask import Markup
 
-DEBUG = True
+
+
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'this is a secret'
 
 class ReusableForm(Form):
-	cname = TextField('Company Name: ',validators=[validators.required()])
+    name = TextField('Name:', validators=[validators.required()])
+ 
+@app.route('/index',methods = ['GET','POST']) 
+@app.route("/", methods=['GET', 'POST'])
+def hello(chartID = 'chart_id',chart_type = ''):
+    form = ReusableForm(request.form)
+ 
+    print (form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        
+ 
+        if form.validate():
+            flash('Hello ' + name)
+        else:
+            flash('Error: All the form fields are required. ')
+   
+ 
+    return render_template('test.html', form=form)
 
-@app.route("/")
-def index():
-	return "Index"
+@app.route('/graph')
+def graph(chartID = 'chart_ID', chart_type = 'Pie_Chart', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+	return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
-@app.route("/hello")
-def hello():
-	return "hello"
+@app.route('/chart')
+def chart():
+	labels = ["January","February","March","April","May","June","July","August"]
+	values = [10,9,8,7,6,4,7,8]
+	colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"  ]
+	return render_template('chart.html', set=zip(values, labels, colors))
 
-@app.route("/tweet",methods=['Get','POST'])
-def tweet():
-	form = ReusableForm(request.form)
-	print(form.errors)
-
-	if request.method == 'POST':
-		name=request.form[cname]
-		print(name)
-
-		if form.validate():
-			flash('Hello ' + name)
-	return "tweet"
-
-@app.route("/tweet/<string:name>/")
-def gettweetname(name):
-	return render_template('test.html',name=name) 
-
-if __name__=="__main__":
-	app.run(host='0.0.0.0',port=80)
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=80,passthrough_errors=True)
